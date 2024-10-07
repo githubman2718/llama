cbd36707a6524c82452b9217e8e75ba14533449e
githubman2718 <60579811+githubman2718@users.noreply.github.com>
2024-10-07 13:42:57 -0400

    Update model.py
    Remove operations with no observable effect.

--- llama/model.py
+++ llama/model.py
@@ -488,7 +488,13 @@ class Transformer(nn.Module):
                 mask
             ]).type_as(h)
 
-        for layer in self.layers:
+        # Do not run the last layer when processing the prompt
+        if seqlen > 1:
+            layers_to_run = self.layers[:-1]  # Exclude the last layer
+        else:
+            layers_to_run = self.layers  # Include all layers
+    
+        for layer in layers_to_run:
             h = layer(h, start_pos, freqs_cis, mask)
         h = self.norm(h)
         output = self.output(h).float()
